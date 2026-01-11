aecf1a93181d592909ebc82f420ba2571fb38b97
xzoytun <zoytunsantosa@gmail.com>
2026-01-11 11:35:22 +0700

    Update generate-qris.js

--- api/generate-qris.js
+++ api/generate-qris.js
@@ -5,26 +5,19 @@ module.exports = async (req, res) => {
   try {
     const { amount, codeqr } = req.query;
 
-    // Cek parameter
     if (!amount || !codeqr) {
       return res.status(400).send('Parameter amount dan codeqr wajib diisi!');
     }
 
-    // 1. Generate QRIS Dynamic string
     const qrisDynamic = await gencode(codeqr, amount);
-
-    // 2. Generate Buffer Gambar
     const qrBuffer = await QRCode.toBuffer(qrisDynamic, {
       margin: 2,
       width: 400
     });
 
-    // 3. Kirim Header Gambar
     res.setHeader('Content-Type', 'image/png');
     res.status(200).send(qrBuffer);
-
   } catch (error) {
-    console.error('SERVER ERROR:', error);
-    res.status(500).send('Terjadi kesalahan internal: ' + error.message);
+    res.status(500).send('Error: ' + error.message);
   }
 };
